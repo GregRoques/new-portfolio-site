8cb19d47018af9877ef21dc3df56380cb234892e
Greg Roques <greg.roques@gmail.com>
2020-12-07 20:48:26 -0500

    update

--- frontend/src/Dependencies/BackendAPI.js
+++
@@ -1,8 +0,0 @@
-export const grAPI = "http://localhost:2000";
-
-//export const grAPI = window.location.href.includes('www.') ? "https://www.gregroques.com/" : "https://gregroques.com/"
-
-// ====== Google Analytics
-export const AuthURL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB8VP9fq09ovmVwLpVlv1Z6RFCraNhYSgM';
-
-export const trackingId = "UA-143112473-1";
