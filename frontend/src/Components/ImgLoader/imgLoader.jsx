10339e521488746eb8fe2c7f910b655962c86d22
Greg Roques <greg.roques@gmail.com>
2021-01-12 09:58:47 -0500

    update

--- frontend/src/Components/ImgLoader/imgLoader.jsx
+++ frontend/src/Components/ImgLoader/imgLoader.jsx
@@ -18,14 +18,14 @@ class ImageLoader extends Component {
     }
 
     render(){
-        const {src, alt, onClick=""} = this.props;
+        const {src, alt, className="", onClick=""} = this.props;
         const {imageLoaded} = this.state;
         return(
             <img 
                 src={src}
                 alt={alt}
                 onClick={onClick}
-                className={`${cssImgLoader.imgLoaderTransition} ${imageLoaded ? cssImgLoader.imgLoaderVisible :  cssImgLoader.imgLoaderHidden } }`}
+                className={`${cssImgLoader.imgLoaderTransition} ${className} ${imageLoaded ? cssImgLoader.imgLoaderVisible :  cssImgLoader.imgLoaderHidden } }`}
                 onDragStart={e=> this.preventImageTheft(e)} 
                 onContextMenu={e=> this.preventImageTheft(e)}
                 onLoad={()=> this.setImageLoaded()}
