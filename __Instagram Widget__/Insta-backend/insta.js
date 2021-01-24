62bb08dcec80819f39ca4fc63c2a64c2195cf8e6
Greg Roques <greg.roques@gmail.com>
2021-01-24 08:33:23 -0500

    update

--- __Instagram Widget__/Insta-backend/insta.js
+++ __Instagram Widget__/Insta-backend/insta.js
@@ -1,6 +1,6 @@
 const instaDefaultLongTermToken = {
     token: "YOUR INITIAL LONG TERM TOKEN GOES HERE",
-    expiration: "new Date().getTime() + 4752000000"
+    expiration: "YOUR TOKEN CREATION DATE PLUS 55 DAYS IN MILLISECONDS GOES HERE"
 } 
 module.exports = instaDefaultLongTermToken;
 
@@ -9,6 +9,6 @@ module.exports = instaDefaultLongTermToken;
 // long term tokens are good for 60 days, and can be refreshed every 24 hours
 
 // ========= get date as milliseconds + 55 days
-// to get new token expiration date + 55 days, use "new Date().getTime() + 4752000000"
+// to get new token expiration date + 55 days, use: "new Date().getTime() + 4752000000"
 
 
