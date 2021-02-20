2d4b16dc34b01e0a60931299b2aff44c65ab1a5e
Greg Roques <greg.roques@gmail.com>
2021-02-20 15:15:42 -0500

    update

--- __Instagram Widget__/Insta-backend/instaImages.js
+++ __Instagram Widget__/Insta-backend/instaImages.js
@@ -71,7 +71,9 @@ const getInstaInfo = () => {
       clearReturnObject();
     })
     .catch((err) => {
-      //console.log(getInstaError)
+      if(err.error.message === "Invalid OAuth 2.0 Access Token"){
+        stopInstaInterval();
+      }
       clearReturnObject(); // we don't want the expired info to remain, so we clear this variable
     });
 };
