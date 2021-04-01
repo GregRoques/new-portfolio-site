a1eb80b155723d4783e1451cdb3fd3368054a462
Greg Roques <greg.roques@gmail.com>
2021-04-01 14:03:43 -0400

    update

--- frontend/src/Components/Media/Design.jsx
+++ frontend/src/Components/Media/Design.jsx
@@ -54,7 +54,7 @@ class Design extends Component{
                 <div className={cssDesign.designDescriptions}>
                 Before becoming a full-time software developer, I worked as a creative in the media/communications field. The skills I developed during this time have been indispensable to my current work. My decade of graphic design and layout experience has allowed me to guide UI/UX best practices; likewise my more than eight years in leadership roles as a manager and director have allowed me to successfully partner with multiple SCRUM-teams within my organization to see projects through to completion.
                 <br/><br/>
-                I continue to contribute freelance works to various publications on a freelance basis. Below is a sample of my published design/layout, photography and writing contributions.
+                I continue to contribute to a variety publications on a freelance basis. Below is a sample of my published design/layout, photography, writing and social content projects.
                 </div>
                 <hr style={{width: '80%', margin: '4rem 10%', color: 'black'}}/>
                 {window.scrollTo(0, 0)}
